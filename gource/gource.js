0f9962bdb3ebfeff45e58bca31d419af6fa38d97
Syed Mohammed Redhan Kamal <impulse96@icloud.com>
2021-02-01 21:10:33 +0530

    Update gource.js
    The gource file that is implemented with the configuration file of gource and enhanced accordingly

--- gource/gource.js
+++ gource/gource.js
@@ -1,13 +1,45 @@
-const { exec } = require("child_process");
+const { exec } = require('child_process')
+const fs = require('fs')
 
-exec("DISPLAY=':99' gource -f -1280x720 -r 25 -o - | ffmpeg -y -r 25 -f image2pipe -vcodec ppm -i - -codec:v libx265 -preset slow -f mp4 commits.mp4", (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
+function runGourceCommand(
+  fileName,
+  options = {
+    DISPLAY_PARAM: "':99'",
+    RES_PARAM: '1280x720',
+    R_PARAM: 25,
+    LIB_PARAM: 'libx265',
+  },
+) {
+  const fileCommand = fs.readFileSync(`./scripts/${fileName}`)
+  try {
+    let cmd = fileCommand.toString()
+    for (let key in options) {
+      cmd = cmd.replace(key, options[key])
+    }
+    console.log('Command that is run -->', cmd)
+
+    const script = exec(cmd)
+    script.stdout.on('data', function (data) {
+      console.log(data.toString())
+    })
+    script.stderr.on('data', function (data) {
+      console.error(data.toString())
+    })
+    script.on('exit', function (code) {
+      console.log('program ended with code: ' + code)
+      return code
+    })
+  } catch (error) {
+    console.error(error)
+    return false
   }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`output: ${stdout}`);
-});
+}
+
+runGourceCommand('gource', {
+  DISPLAY_PARAM: "':22'",
+  RES_PARAM: '1280x720',
+  R_PARAM: 25,
+  LIB_PARAM: 'libx265',
+})
+
+module.exports = runGourceCommand
