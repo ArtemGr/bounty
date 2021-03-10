549cc6c94eb67b1d9beea9eead5a1c8d53ecfcfc
Artemciy <artemciy@gmail.com>
2021-03-10 09:22:25 +0300

    Document step one of Google Drive prototype

---
+++ google-drive/google-drive.js
@@ -0,0 +1,6 @@
+//@ts-check
+
+// TODO: Figure how to allow for invoking this script from the command line while ALSO using it as a library with exports?
+// (filed as https://github.com/ArtemGr/bounty/issues/23)
+
+
