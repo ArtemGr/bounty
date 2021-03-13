122f15efcb5b68810042e99f673793461b260207
Artemciy <artemciy@gmail.com>
2021-03-13 12:43:24 +0300

    Acquite notes

--- acqui/acqui.js
+++ acqui/acqui.js
@@ -1,28 +1,93 @@
 //@ts-check
 
-const date = require('date-and-time');  // https://www.npmjs.com/package/date-and-time
+const date = require ('date-and-time');  // https://www.npmjs.com/package/date-and-time
 const fs = require ('fs'); const fsp = fs.promises;
 const os = require ('os');
+// https://nodejs.org/dist/latest-v15.x/docs/api/readline.html
+const readline = require ('readline');
 // https://github.com/cronvel/terminal-kit/blob/master/doc/documentation.md
 // https://github.com/cronvel/terminal-kit/blob/master/doc/high-level.md
 const termkit = require ('terminal-kit');
 const {log} = require ('log');
+const yaml = require ('yaml');  // https://github.com/eemeli/yaml
+const {YAMLMap} = require ('yaml/types');
+
+const HOME = os.homedir()
+
+// TODO: Consider using a readable time representation instead
+
+/**
+ * @typedef {Object} Note
+ * @property {string} item Some human-readable item identifier, provided by user
+ * @property {string[]} tags Additions to DP timeline and/or to folksonomy
+ * @property {number} sec Time of data acquisition, in seconds since UNIX epoch
+ * @property {string} [note] Free-form text note about the `item`
+ */
+
+/** @returns {Promise<Note[]>} Loaded from “~/notes.yaml” */
+async function loadNotes() {
+  // cf. https://stackoverflow.com/questions/14391690/how-to-capture-no-file-for-fs-readfilesync
+  let notesˢ
+  try {
+    notesˢ = await fsp.readFile (HOME + '/notes.yaml', 'utf8')
+  } catch (err) {
+    if (err.code == 'ENOENT') notesˢ = '[]'
+    else throw err}
+  if (notesˢ === '') notesˢ = '[]'
+  return yaml.parse (notesˢ)}
+
+/** @param {Note[]} notes For “~/notes.yaml” */
+async function saveNotes (notes) {
+  const notesᵈ = new yaml.Document (notes)
+
+  if (notes.length != 0) {  // Document the database with YAML comments
+    /** @type {YAMLMap} */
+    const orderᵐ = notesᵈ.get (0, true)
+    orderᵐ.commentBefore = ' Array of notes, updated from acqui.js'
+    orderᵐ.get ('item', true) .commentBefore = ' Some human-readable item identifier, provided by user'
+    orderᵐ.get ('tags', true) .commentBefore = ' Additions to DP timeline and/or to folksonomy'
+    orderᵐ.get ('sec', true) .commentBefore = ' Time of data acquisition, in seconds since UNIX epoch'}
+
+  const notesᵖ = HOME + '/notes.yaml'
+  log (notesᵖ)
+  const notesᵗ = notesᵖ + '.' + Date.now() + '.tmp'
+  await fsp.writeFile (notesᵗ, notesᵈ.toString())
+  await fsp.rename (notesᵗ, notesᵖ)}
+
+/**
+ * Adds a note to the YAML array.  
+ * We'll likely switch the “timeline” to the notes array later.
+ * @param {string} item Some human-readable item identifier, provided by user
+ * @param {Set<string>} tags Additions to DP timeline and/or to folksonomy
+ * @param {string} [noteˢ] Free-form text note about the `item`
+ */
+async function fileNote (item, tags, noteˢ) {
+  log (`item: ${item}; tags: ${[...tags]}; note: ${noteˢ}`)
+  const notes = await loadNotes()
+
+  /** @type {Note} */
+  const note = {item: item, tags: [...tags], sec: Math.floor (Date.now() / 1000)}
+  if (typeof noteˢ !== 'undefined') note.note = noteˢ
+
+  notes.push (note)
+  await saveNotes (notes)}
 
 (async function() {
 
   const term = termkit.terminal
   log.out = (line, loc) => {loc && term.gray (loc + '] '); term.gray (line); term ('\n')}
 
+  term.windowTitle ('acquisition')
+
   log ('Loading timeline…')
-  const home = os.homedir()
-  const timelineᵖ = fs.existsSync (home + '/timeline') ? home + '/timeline' : home + '/Downloads/timeline';
+  const timelineᵖ = fs.existsSync (HOME + '/timeline') ? HOME + '/timeline' : HOME + '/Downloads/timeline';
   const timeline = await fsp.readFile (timelineᵖ, {encoding: 'utf8'})
   let ca, re = /(20\d{12}):\s(\[[\w,]+\]\s)?(.*?)(?=\n20\d{12}|\n$|$)/g, lines = new Set()
   while (ca = re.exec (timeline)) lines.add (ca[3])
 
   term ('> ')  // PS2
   // https://github.com/cronvel/terminal-kit/blob/master/doc/high-level.md#ref.inputField
-  const line = await term.inputField ({
+  let line = await term.inputField ({
     cancelable: true,
     history: [...lines],  // For UP and DOWN
     autoComplete: [...lines],
@@ -37,11 +102,17 @@ const {log} = require ('log');
   }).promise
 
   if (typeof line == 'undefined') {term.processExit (0); return}
+  line = line.trim()
+  if (line === '') {term.processExit (0); return}
 
   const tags = new Set()
   for (;;) {
     term ('\n> ')
-    const tagsʰ = ['fireworks', 'entropy', 'orientation', 'wellness', 'family', 'mv']
+    const tagsʰ = [
+      'fireworks', 'entropy', 'orientation', 'wellness',  // DP
+      'family',
+      'mv',
+      'note']
     const tag = await term.inputField ({
       cancelable: true,
       history: tagsʰ,  // For UP and DOWN
@@ -51,6 +122,21 @@ const {log} = require ('log');
     }).promise
     if (typeof tag == 'undefined') {term.processExit (0); return}
     if (tag == '') break
+    // The “notes” tag separates the DP tracking of activities from the generic data acquisition
+    if (tag == 'note') {
+      // Tried to use the “readline” there instead of the “inputField” to see whether it'll
+      // allow for swipe keyboard under Termux. It does not.
+      // But we can swipe left on the extra buttons in order to switch to the
+      // [“Text Input View”](https://wiki.termux.com/wiki/Touch_Keyboard)
+      const rl = readline.createInterface ({input: process.stdin, output: process.stdout})
+      const question = new Promise (resolve => rl.question ('Enter the note line below:\n', resolve))
+      let note = await question
+      note = note.trim()
+      if (note === '') {term.processExit (0); return}
+      await fileNote (line, tags, note)
+      rl.close()
+      term.processExit (0); return
+    }
     tags.add (tag)
   }
 
