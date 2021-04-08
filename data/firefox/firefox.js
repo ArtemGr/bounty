e716999d5ee77a48b20eb75cf6d84b3f736e1f94
Artemciy <artemciy@gmail.com>
2021-04-08 14:51:16 +0300

    Unpack jsonlz4

--- data/firefox/firefox.js
+++ data/firefox/firefox.js
@@ -5,9 +5,10 @@
 const {assert} = require ('console');
 const fs = require ('fs');
 const fsp = fs.promises;
-const {knuthShuffle} = require ('knuth-shuffle');  // https://stackoverflow.com/a/2450976/257568
-const os = require ('os');
+const lz4 = require ('lz4');
 const {log, snooze} = require ('log');
+const os = require ('os');
+const {knuthShuffle} = require ('knuth-shuffle');  // https://stackoverflow.com/a/2450976/257568
 
 class Tab {
   /**
@@ -42,18 +43,34 @@ exports.profiles = async function() {
 exports.tabs = async function() {
   const profiles = await exports.profiles()
   const profilesʹ = await fsp.readdir (profiles)
+  if (!profilesʹ.length) throw new Error (`No profiles at "${profiles}"`)
 
   // Sort the profiles by last-modified, in order to pick the recent/actual one
-  const lm = /** @type {Map<String, number>} */ (new Map())
-  const lmᶠ = /** @param {string} fname */ fname => {
-    let ms = lm.get (fname)
+  const lm = /** @type {Map<string, number>} */ (new Map())
+  const lmᶠ = /** @param {string} pname */ pname => {
+    let ms = lm.get (pname)
     if (!ms) {
-      const stat = fs.statSync (`${profiles}/${fname}`)
-      const backups = fs.existsSync (`${profiles}/${fname}/sessionstore-backups`)
-      ms = stat.isDirectory() && backups ? stat.mtimeMs : -1
-      lm.set (fname, ms)}
+      const pstat = fs.statSync (`${profiles}/${pname}`)
+      const bexists = fs.existsSync (`${profiles}/${pname}/sessionstore-backups`)
+      ms = pstat.isDirectory() && bexists ? pstat.mtimeMs : -1
+      lm.set (pname, ms)}
     return ms}
   profilesʹ.sort ((a, b) => lmᶠ (b) - lmᶠ (a))
+
+  const pname = profilesʹ[0]
+  const recoveryᵖ = `${profiles}/${pname}/sessionstore-backups/recovery.jsonlz4`
+  log (recoveryᵖ)
+  const lz4bytes = await fsp.readFile (recoveryᵖ)
+  // cf. https://github.com/toashd/mozlz4/blob/d94719e5bae9e9dbd3e64847f91ab61e8646ad8d/index.js#L30
+  const magic = 'mozLz40\0'
+  assert (lz4bytes.slice (0, 8) .toString ('utf8') == magic)
+  const lz4len = lz4bytes.readUInt32LE (8)  // bytes 8..12
+  const lz4buf = Buffer.alloc (lz4len)
+  const lz4lenʹ = lz4.decodeBlock (lz4bytes, lz4buf, 12)
+  assert (lz4len == lz4lenʹ)
+  const lz4str = new TextDecoder ('utf8') .decode (lz4buf)
+  const recovery = JSON.parse (lz4str)
+  log (recovery)
 
   return []}
 
