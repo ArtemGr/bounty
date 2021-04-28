95f9f067b7d0f9313ddefd558334d39d50ec6776
Artemciy <artemciy@gmail.com>
2021-04-28 09:55:31 +0300

    Load WARC paths

--- data/common-crawl/common-crawl.rs
+++ data/common-crawl/common-crawl.rs
@@ -3,9 +3,15 @@
 #[macro_use] extern crate fomat_macros;
 #[macro_use] extern crate gstuff;
 
+use std::io::Read;
+
 use chrono::{Local, TimeZone};
 use chrono::format::DelayedFormat;
 use chrono::format::strftime::StrftimeItems;
+use flate2::read::GzDecoder;
+
+macro_rules! status {($($args: tt)+) => {if *::gstuff::ISATTY {
+  ::gstuff::status_line (file!(), line!(), fomat! ($($args)+))}}}
 
 fn short_log_time (ms: u64) -> DelayedFormat<StrftimeItems<'static>> {
   let time = Local.timestamp_millis (ms as i64);
@@ -18,6 +24,16 @@ macro_rules! log {($($args: tt)+) => {{
       (::gstuff::filename (file!())) ':' (line!()) "] "
       $($args)+);})}}}
 
-fn main() {
-  log! ("hello world")
-}
+fn main() -> Result<(), String> {
+  let pathsᵘ = "https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2021-10/warc.paths.gz";
+  status! ("Loading " (pathsᵘ) "…");
+  let rc = try_s! (attohttpc::get (pathsᵘ) .send());
+  assert! (rc.headers().contains_key ("x-amz-storage-class"));
+  assert! (rc.headers().contains_key ("etag"));
+  assert! (rc.headers().get ("server") .map (|v| v.as_bytes() == b"AmazonS3") .unwrap_or (false));
+  let pathsᵇ = try_s! (rc.bytes());
+  let mut pathsᶻ = GzDecoder::new (&pathsᵇ[..]);
+  let mut paths = String::new();
+  try_s! (pathsᶻ.read_to_string (&mut paths));
+  log! ((paths));
+  Ok(())}
