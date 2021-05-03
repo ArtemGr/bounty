db54c06774598074c9fb302badc4945d04b6f3e8
Artemciy <artemciy@gmail.com>
2021-05-03 20:15:55 +0300

    Update with just "drive.file"

--- data/google-drive/google-drive.js
+++ data/google-drive/google-drive.js
@@ -2,6 +2,7 @@
 
 const {spawnSync} = require ('child_process');
 const clipboardy = require ('clipboardy');
+const {assert} = require ('console');
 const fs = require ('fs'); const fsp = fs.promises;
 const {google} = require ('googleapis');  // https://www.npmjs.com/package/googleapis
 const {log, snooze} = require ('log');
@@ -17,10 +18,7 @@ async function findSecret() {
   const files = await fsp.readdir ('.')
   return files.find (f => /^client_secret.*\.json$/ .test (f))}
 
-/** Demonstrate creating a Google Drive file */
-async function pocCreate() {
-  // See “step one: get secret” in “README.md” for how to obtain client_secret*.json
-
+async function secret2oauth() {
   const secretⁿ = await findSecret()
   if (!secretⁿ) throw new Error ('No client_secret*.json')
   const secretˢ = await fsp.readFile (secretⁿ, {encoding: 'utf8'})
@@ -32,15 +30,13 @@ async function pocCreate() {
   if (!i.redirect_uris) throw new Error ('No “installed.redirect_uris in client_secret')
   // https://github.com/googleapis/google-api-nodejs-client#generating-an-authentication-url
   const oauth2 = new google.auth.OAuth2 (i.client_id, i.client_secret, i.redirect_uris[0])
+  return oauth2}
 
-  // First we need the full “https://www.googleapis.com/auth/drive” scope
-  // to create the file from the app,
-  // then later the limited “drive.file” scope should work for updates
+const timeline = fs.createReadStream ('c:/spool/synced/iPhone/timeline.webm')
 
-  const timeline = fs.createReadStream ('c:/spool/synced/iPhone/timeline.mp4')
-
-  if (fs.existsSync ('full-token.json')) {
-    const tokenˢ = await fsp.readFile ('full-token.json', {encoding: 'utf8'})
+async function scope2token (oauth2, scope, file) {
+  if (fs.existsSync (file)) {
+    const tokenˢ = await fsp.readFile (file, {encoding: 'utf8'})
     /** @type {Credentials} */
     const tokenʲ = JSON.parse (tokenˢ)
     oauth2.credentials = tokenʲ
@@ -48,7 +44,7 @@ async function pocCreate() {
     const authUrl = oauth2.generateAuthUrl ({
       access_type: 'offline',
       // https://developers.google.com/drive/api/v3/about-auth
-      scope: 'https://www.googleapis.com/auth/drive',
+      scope: scope,
       prompt: 'consent'})
 
     if (win) {
@@ -70,22 +66,33 @@ async function pocCreate() {
     let token = null
     while (input == null) {
       await snooze (31)
-      const clipboard = (await clipboardy.read()).trim()
+      const clipboard = (await clipboardy.read()) .trim()
       if (clipboard == clipboardₒ) continue
-      log (clipboard)
       clipboardₒ = clipboard
-      try {
+      log (clipboard)
+      try {  // See if something we've got from clipboard works as the code
         token = await oauth2.getToken (clipboard)
         ie.abort()
         break
       } catch (ex) {log (ex)}}
     if (token == null && input != null) token = await oauth2.getToken (input)
-    log (token)
+    log (token.tokens)
+    assert (token.tokens.scope == scope)
 
     await fsp.rm ('auth.url')
-    await fsp.writeFile ('full-token.json', JSON.stringify (token.tokens))
-    oauth2.credentials = token.tokens}
+    await fsp.writeFile (file, JSON.stringify (token.tokens))
+    oauth2.credentials = token.tokens}}
 
+/** Demonstrate creating a Google Drive file */
+async function pocCreate() {
+  // See “step one: get secret” in “README.md” for how to obtain client_secret*.json
+  const oauth2 = await secret2oauth()
+
+  // First we need the full “https://www.googleapis.com/auth/drive” scope
+  // to create the file from the app,
+  // then later the limited “drive.file” scope should work for updates
+
+  await scope2token (oauth2, 'https://www.googleapis.com/auth/drive', 'full-token.json')
   const drive = google.drive ({version: 'v3', auth: oauth2})
 
   // https://developers.google.com/drive/api/v3/reference/files/list
@@ -105,9 +112,9 @@ async function pocCreate() {
     requestBody: {
       name: 'timeline.mp4',
       description: 'Full Gource timeline',
-      mimeType: 'video/mp4'},
+      mimeType: 'video/webm'},
     media: {
-      mimeType: 'video/mp4',
+      mimeType: 'video/webm',
       body: timeline},
     // https://developers.google.com/drive/api/v3/fields-parameter
     fields: 'id, webViewLink'})
@@ -128,10 +135,26 @@ async function pocCreate() {
 
 /** Demonstrate updating a Google Drive file with but the minimal “drive.file” token */
 async function pocUpdate() {
-  // scope: 'https://www.googleapis.com/auth/drive.file',
-  // ⌥ load the limited “drive.file” token
-  // ⌥ save the limited “drive.file” token
-}
+  const oauth2 = await secret2oauth()
+  await scope2token (oauth2, 'https://www.googleapis.com/auth/drive.file', 'drive-file-token.json')
+  const drive = google.drive ({version: 'v3', auth: oauth2})
+
+  const have = await drive.files.list ({
+    q: "name='timeline.mp4'"})
+  assert (have.data.files.length)
+  const fid = have.data.files[0].id
+
+  // https://developers.google.com/drive/api/v3/reference/comments/update
+  const rc = await drive.files.update ({
+    fileId: fid,
+    media: {
+      mimeType: 'video/webm',
+      body: timeline},
+    fields: 'webViewLink'})
+
+  log (fid)
+  log (`https://drive.google.com/uc?id=${fid}`)  // Direct video stream
+  log (rc.data.webViewLink)}
 
 async function clean() {
   const rm = async name => {if (name && fs.existsSync (name)) await fsp.rm (name)}
