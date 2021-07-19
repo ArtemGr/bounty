c138b52cce305451f03973cb8926c8ae1a7cca7e
Artemciy <artemciy@gmail.com>
2021-07-20 02:11:00 +0300

    Reuse tfjs-examples-mnist-node to download MNIST

--- data/elm/elm.js
+++ data/elm/elm.js
@@ -2,40 +2,37 @@
 
 // https://www.kaggle.com/robertbm/extreme-learning-machine-example
 
-// ⌥ Experiment with importing CommonJS code from ES module,
-//    cf. https://nodejs.org/api/packages.html
-// ⌥ See if we can import https://github.com/tensorflow/tfjs-examples/blob/master/mnist-node/data.js
+// https://youtu.be/hqvAfLlmWw4 ES modules (in Russian)
+// 
 
-// https://youtu.be/hqvAfLlmWw4 ES modules
+//import * as tf from '@tensorflow/tfjs';
 
-import * as tf from '@tensorflow/tfjs';
-
-/*
-
-const {assert} = require ('console');
-const date = require ('date-and-time');
-const fs = require ('fs');
+// https://github.com/tensorflow/tfjs-examples/blob/master/mnist-node/data.js
+import * as mnistʹ from 'tfjs-examples-mnist-node/data.js';
+import {log} from 'log';
+import fs from 'fs';
 const fsp = fs.promises;
-const {knuthShuffle} = require ('knuth-shuffle');  // https://stackoverflow.com/a/2450976/257568
-const {log, snooze} = require ('log');
-const os = require ('os');
 
+async function loadMnist() {
+  // @ts-ignore
+  const mnist = /** @type {mnistʹ} */ (mnistʹ.default)
 
+  const data = await mnist.loadData()
+  log (data)
+}
 
-exports.test = async function() {
-  log (`TBD`)}
+async function clean() {
+  await fsp.unlink ('t10k-images-idx3-ubyte')
+  await fsp.unlink ('t10k-labels-idx1-ubyte')
+  await fsp.unlink ('train-images-idx3-ubyte')
+  await fsp.unlink ('train-labels-idx1-ubyte')}
 
 function help() {
-  console.log ('npm i && node elm.js --TBD')}
+  console.log ('node elm.js --load-mnist')
+  console.log ('node elm.js --clean')}
 
-// When invoked from console, “npm i && node elm.js --tracks”
-// cf. https://nodejs.org/dist/latest-v15.x/docs/api/modules.html#modules_accessing_the_main_module
-if (require.main === module) (async () => {
+(async () => {
   if (process.argv.includes ('--help')) {help(); return}
-
-  if (process.argv.includes ('--TBD')) {
-    log (`TBD`)
-    return}
+  if (process.argv.includes ('--load-mnist')) {await loadMnist(); return}
+  if (process.argv.includes ('--clean')) {await clean(); return}
 })()
-
-*/
