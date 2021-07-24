6acb49bf2913a57a4c2c5cfdcc75d09a635fa765
Artemciy <artemciy@gmail.com>
2021-07-24 18:29:28 +0300

    Run tfjs-examples-mnist-node on copied tensors

--- data/elm/elm.js
+++ data/elm/elm.js
@@ -4,6 +4,7 @@
 
 // https://youtu.be/oq3tq-0gxOs importing and loading MNIST
 // https://youtu.be/AhmSUmrAkeQ solving dependency duplication due to version mismatch
+// https://youtu.be/0mcyw5uapWc running tfjs-examples-mnist-node on copied tensors
 
 const crypto = require ('crypto');
 const fs = require ('fs'); const fsp = fs.promises;
@@ -52,6 +53,46 @@ async function loadInt (/** @type {fs.promises.FileHandle} */ dump) {
   const buf = await loadChunk (dump)
   return new Int32Array (buf.buffer)}
 
+function modelʹ() {
+  const model = tf.sequential();
+  model.add(tf.layers.conv2d({
+    inputShape: [28, 28, 1],
+    filters: 32,
+    kernelSize: 3,
+    activation: 'relu',
+  }));
+  model.add(tf.layers.conv2d({
+    filters: 32,
+    kernelSize: 3,
+    activation: 'relu',
+  }));
+  model.add(tf.layers.maxPooling2d({poolSize: [2, 2]}));
+  model.add(tf.layers.conv2d({
+    filters: 64,
+    kernelSize: 3,
+    activation: 'relu',
+  }));
+  model.add(tf.layers.conv2d({
+    filters: 64,
+    kernelSize: 3,
+    activation: 'relu',
+  }));
+  model.add(tf.layers.maxPooling2d({poolSize: [2, 2]}));
+  model.add(tf.layers.flatten());
+  model.add(tf.layers.dropout({rate: 0.25}));
+  model.add(tf.layers.dense({units: 512, activation: 'relu'}));
+  model.add(tf.layers.dropout({rate: 0.5}));
+  model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+
+  const optimizer = 'rmsprop';
+  model.compile({
+    optimizer: optimizer,
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy'],
+  });
+
+  return model}
+
 async function tbd() {
   const dump = await fsp.open ('dump.ns', 'r')
 
@@ -62,8 +103,8 @@ async function tbd() {
   //log (images)
 
   log (`Loading trainLabelsData..`)
-  const trainLabelsData = await loadInt (dump)
-  const labels = tf.oneHot (tf.tensor1d (trainLabelsData, 'int32'), 10) .toFloat()
+  const trainLabelsData = await loadFloat (dump)
+  const labels = tf.tensor2d (trainLabelsData, [60000, 10])
   //log (labels)
 
   log (`Loading testImagesData..`)
@@ -73,11 +114,34 @@ async function tbd() {
   //log (testImages)
 
   log (`Loading testLabelsData..`)
-  const testLabelsData = await loadInt (dump)
-  const testLabels = tf.oneHot (tf.tensor1d (testLabelsData, 'int32'), 10) .toFloat()
+  const testLabelsData = await loadFloat (dump)
+  const testLabels = tf.tensor2d (testLabelsData, [10000, 10])
   log (testLabels)
 
   await dump.close()
+
+  // running tfjs-examples-mnist-node on copied tensors
+
+  const model = modelʹ();
+  model.summary();
+
+  const validationSplit = 0.15;
+  const numTrainExamplesPerEpoch = images.shape[0] * (1 - validationSplit);
+  const batchSize = 128;
+  const epochs = 3;
+  const numTrainBatchesPerEpoch = Math.ceil (numTrainExamplesPerEpoch / batchSize);
+  await model.fit (images, labels, {
+    epochs,
+    batchSize,
+    validationSplit
+  });
+
+  const evalOutput = model.evaluate (testImages, testLabels);
+
+  console.log(
+      `\nEvaluation result:\n` +
+      `  Loss = ${evalOutput[0].dataSync()[0].toFixed(3)}; `+
+      `Accuracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`);
 }
 
 function help() {
