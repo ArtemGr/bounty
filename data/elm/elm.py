f2e1c447256f9a057301123ae5e6c3d10b0562e8
Artemciy <artemciy@gmail.com>
2022-02-08 01:38:38 +0300

    Plot the function learned for id and inverse

--- data/elm/elm.py
+++ data/elm/elm.py
@@ -15,7 +15,7 @@ from llog import log
 rng = np.random.default_rng()
 
 
-def build_h(input, weights, bias):
+def build_h(inputs, weights, bias):
   '''
   ```
   H = [
@@ -36,25 +36,25 @@ def build_h(input, weights, bias):
   '''
   ᶰ = len(weights)
   h = []
-  for sample in range(0, len(input)):
+  for sample in range(0, len(inputs)):
     row = []
     for neuron in range(0, ᶰ):
       # The neuron then applies an activation function to the “sum of weighted inputs”
       wx = 0
-      for x in input[sample]:
+      for x in inputs[sample]:
         wx += weights[neuron] * x
       row.append(math.sin(wx + bias[neuron]))
     h.append(row)
   return h
 
 
-def train(ᶰ, input, output):
+def train(ᶰ, inputs, outputs):
   weights = rng.standard_normal(ᶰ, dtype=np.float32)
   bias = rng.standard_normal(ᶰ, dtype=np.float32)
-  h = build_h(input, weights, bias)
+  h = build_h(inputs, weights, bias)
   hꜝ = np.linalg.pinv(h)
   # β = H†T, where † is Moore–Penrose inverse
-  β = np.matmul(hꜝ, output)
+  β = np.matmul(hꜝ, outputs)
   return (weights, bias, β)
 
 
@@ -85,22 +85,42 @@ def floorᵃ(a):
 
 
 if __name__ == '__main__':
-  (weights, bias, β) = train(2, [[1], [2]], [[1], [2]])
+  inputs = [[1], [2]]
+  outputs = [[1], [2]]
+  weights, bias, β = train(2, inputs, outputs)
   log('id (1) =', infer(weights, bias, β, [1]))
   log('id (2) =', infer(weights, bias, β, [2]))
 
-  (weights, bias, β) = train(3, [[1], [2], [3]], [[3], [2], [1]])
+  import matplotlib.pyplot as plt
+  import pylab
+
+  def plot():
+    xs = []
+    ys = []
+    for i in np.linspace(-1, 7, 44):
+      xs.append(i)
+      ys.append(infer(weights, bias, β, [i])[0])
+    plt.plot(xs, ys, label='id')
+    plt.scatter(inputs, outputs)
+    pylab.show()
+
+  plot()
+
+  inputs = [[1], [2], [3]]
+  outputs = [[3], [2], [1]]
+  weights, bias, β = train(3, inputs, outputs)
   log('inverse (1) =', infer(weights, bias, β, [1]))
   log('inverse (2) =', infer(weights, bias, β, [2]))
   log('inverse (3) =', infer(weights, bias, β, [3]))
+  plot()
 
-  (weights, bias, β) = train(3, [[2, 2], [3, 3], [2, 3]], [[4], [6], [5]])
+  weights, bias, β = train(3, [[2, 2], [3, 3], [2, 3]], [[4], [6], [5]])
   log('sum (2, 2) =', infer(weights, bias, β, [2, 2]))
   log('sum (3, 3) =', infer(weights, bias, β, [3, 3]))
   log('sum (2, 3) =', infer(weights, bias, β, [2, 3]))
 
   # https://en.wikipedia.org/wiki/Euclidean_division
-  (weights, bias, β) = train(3, [[7, 3], [9, 4], [9, 3]], [[2, 1], [2, 1], [3, 0]])
+  weights, bias, β = train(3, [[7, 3], [9, 4], [9, 3]], [[2, 1], [2, 1], [3, 0]])
   log('7 / 3 =', infer(weights, bias, β, [7, 3]))
   log('9 / 4 =', infer(weights, bias, β, [9, 4]))
   log('9 / 3 =', floorᵃ(infer(weights, bias, β, [9, 3])))
