39c8401a3b830cf336bffeb58dc9c46b13b06632
Artemciy <artemciy@gmail.com>
2022-03-24 23:29:35 +0300

    Plot perfect symmetry

--- data/math/xgboost-.py
+++ data/math/xgboost-.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
 import os
+import shutil
 from math import sqrt
 
+import numpy as np
 import xgboost as xgb
-from keras.datasets import mnist
-from llog import floorʹ, log
+from llog import floorʹ, log, plot
 
-if __name__ == '__main__':
+
+def mnist():
+  from keras.datasets import mnist
+
   log('loading MNIST…')
   (x_train, y_train), (x_test, y_test) = mnist.load_data()
   assert os.path.exists(os.path.expanduser('~/.keras/datasets/mnist.npz'))
@@ -52,3 +56,56 @@ if __name__ == '__main__':
   mse = loss / len(ypred)
   # cf. error rates at http://yann.lecun.com/exdb/mnist/
   log('MSE', floorʹ(mse), 'RMSE', floorʹ(sqrt(mse)))
+
+
+def plotio(inputs, outputs, xs):
+  wh = shutil.get_terminal_size((111, 11))
+  wh = (wh.columns - 1, min(3, wh.lines - 3))
+  param = {'max_depth': 2, 'eta': 1, 'objective': 'reg:squarederror'}
+  for rounds in range(1, 9):
+    best = xgb.train(param, xgb.DMatrix(inputs, label=outputs), num_boost_round=rounds)
+    ys = list(best.predict(xgb.DMatrix([[x]]))[0] for x in xs)
+    log(rounds, xs, ys)
+    a = [[' ' for x in range(wh[0])] for u in range(wh[1])]
+    map = plot(a, xs, ys)
+    for *_, ax, ay in map.zip([i[0] for i in inputs], [o[0] for o in outputs]):
+      a[ay][ax] = '\033[34m*\033[0m'
+    print('\n'.join(''.join(y) for y in a))
+
+
+def perfect_symmetry():
+  '''
+  “a nice example of data with "perfect symmetry" with unstable balance.
+  With such perfect dataset, when the algorithm is looking for a split, say in variable X1,
+  the sums of residuals at each x1 location of it WRT X2 are always zero,
+  thus it cannot find any split and is only able to approximate the total average”
+  https://github.com/dmlc/xgboost/issues/4069#issuecomment-456266057
+  https://www.mariofilho.com/can-gradient-boosting-learn-simple-arithmetic/
+  '''
+
+  import matplotlib.pyplot as plt
+
+  X = np.zeros((size := 10000, 2))
+  Z = np.meshgrid(np.linspace(-1, 1, 100), np.linspace(-1, 1, 100))
+  X[:, 0] = Z[0].flatten()
+  X[:, 1] = Z[1].flatten()
+  y_mul = X[:, 0] * X[:, 1]
+
+  fig = plt.figure(figsize=(5, 5))
+  ax = fig.add_subplot(2, 1, 1, projection='3d')
+  ax.plot_trisurf(X[:, 0], X[:, 1], y_mul, cmap='viridis')
+
+  dtrain = xgb.DMatrix(X, label=y_mul)
+  params = {'tree_method': 'gpu_hist'}
+  bst = xgb.train(params, dtrain, evals=[(dtrain, 'train')], num_boost_round=10)
+  y_pred = bst.predict(dtrain)
+
+  ax = fig.add_subplot(2, 1, 2, projection='3d')
+  ax.plot_trisurf(X[:, 0], X[:, 1], y_pred, cmap='viridis')
+  plt.show()
+
+
+if __name__ == '__main__':
+  #plotio([[1], [2]], [[1], [2]], np.linspace(-1, 4, 6))  # id
+  #plotio([[1], [2], [3]], [[3], [2], [1]], np.linspace(-1, 5, 7))  # inverse
+  perfect_symmetry()
