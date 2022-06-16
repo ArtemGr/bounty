0f894ed5562435c599ac8cc12aa6daffcf8b26ef
Vyacheslav V Sokolov <deathless@t-sk.ru>
2022-06-17 03:29:07 +0700

    https://github.com/ArtemGr/bounty/issues/38

--- data/common-crawl/csv-test.py
+++ data/common-crawl/csv-test.py
@@ -8,11 +8,45 @@ from llog import log
 # https://play.rust-lang.org/?version=nightly&mode=debug&edition=2021&gist=8f7bd61f2c992fb9a3225c11d0b4aaec
 # Resulting port should pass the unit test below:
 
+import time
+
+
+chars_to_csv = {
+    "\x01": "\u0001\u0001",
+    "\x00": "\u0001\u0002",
+    "\n": "\u0001\u0003",
+    "\r": "\u0001\u0004",
+    "\"": "\u0001\u0005",
+    ",": "\u0001\u0006"
+}
+
+
 def csesc (fr):
-  return b'foo'
+    result = []
+    for c in fr:
+        result.append(chars_to_csv.get(c, c))
+    return "".join(result)
+
+
+csv_to_chars = ["\x01", "\x00", "\n", "\r", "\"", ","]
+
 
 def csunesc (fr):
-  return b'bar'
+    result = []
+    encoded = False
+    for c in fr:
+        if encoded:
+            encoded = False
+            if c < "\u0007":
+                result.append(csv_to_chars [ord (c) - 1])
+            else:
+                result.append("\u0001" + c)
+        elif c == "\u0001":
+            encoded = True
+        else:
+            result.append(c)
+    return "".join(result)
+            
 
 if __name__ == '__main__':
   assert csesc ("Привет, Юля!") == "Привет\u0001\u0006 Юля!"
